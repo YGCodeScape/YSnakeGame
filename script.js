a83a1bd065a7a604d27d92e1fca1dfa687eb5bb9
yashG <yrg.creation.ai@gmail.com>
2025-11-29 20:32:46 +0530

    snake logic

--- script.js
+++ script.js
@@ -6,13 +6,63 @@ const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
 
 const blocks = []
+const snake = [
+    {x: 0, y: 3}
+]
+let direction = 'down'
 
 for(let r = 0; r < rows; r++ ) {
     for(let c = 0; c < cols; c++) {
         const block = document.createElement('div');
         block.classList.add('block');
         board.appendChild(block);  
-        block.innerText = `${r}${c}`;
+        // block.innerText = `${r}${c}`;
         blocks[`${r}-${c}`] = block;
     }
 }
+
+function render() {
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    })
+}
+
+setInterval(() => {
+    let head = null
+    if(direction === "left") {
+        head = {x: snake[0].x, y: snake[0].y-1}
+    }
+    else if(direction === "right") {
+        head = {x: snake[0].x, y: snake[0].y+1}
+    }
+    else if(direction === "up") {
+        head = {x: snake[0].x-1, y: snake[0].y}
+    }
+    else if(direction === "down") {
+        head = {x: snake[0].x+1, y: snake[0].y}
+    }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill");        
+    })
+
+    snake.unshift(head)
+    snake.pop()
+
+    render()
+}, 400)
+
+addEventListener('keydown', (event) => {
+    if(event.key === "ArrowLeft") {
+        direction = "left"
+    }
+    else if(event.key === "ArrowRight") {
+        direction = "right"
+    }
+    else if(event.key === "ArrowUp") {
+        direction = "up"
+    }
+    else if(event.key === "ArrowDown") {
+        direction = "down"
+    }
+})
